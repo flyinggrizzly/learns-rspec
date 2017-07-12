d4d599bcf010d185a37ebe0d42a62cbd872bff22
SeanDMR <sean@flyinggrizzly.io>
2017-07-12 20:42:07 +0100

    01-02: RED: pending examples fail because they can now pass but are still marked as pending

--- 02-running-specs/spec/coffee_spec.rb
+++ 02-running-specs/spec/coffee_spec.rb
@@ -10,6 +10,20 @@ class Coffee
   def price
     1.00 + ingredients.size * 0.25
   end
+
+  # This causes the coffee color example to start passing
+  # which means that the test suite will flag it by 'failing' the passing test
+  # to bring it to our attention
+  def color
+    ingredients.include?(:milk) ? :light : :dark
+  end
+
+  # This causes the coffee temp example to start passing
+  # which means that the test suite will flag it by 'failing' the passing test
+  # to bring it to our attention
+  def temp
+    ingredients.include?(:milk) ? 190.0 : 205.0
+  end
 end
 
 # Enables --only-failures flag for RSpec CLI
@@ -24,15 +38,6 @@ RSpec.describe 'A cup of coffee' do
   # Tagged: run just this example with `rspec --tag milky:false`
   it 'costs $1', milky: false do
     expect(coffee.price).to eql(1.00)
-  end
-
-  # Pending example with expectation to be skipped
-  it 'is cooler than 200F' do
-    # Including the pending helper means that any code below it will be skipped
-    # Any code above it is still expected to pass
-    # You should include an explanation why the example is still pending
-    pending 'temperature not implemented yet'
-    expect(coffee.temp).to be < (200)
   end
 
   # context is an alias for describe, but makes it clear when describing a subset of behaviors
@@ -50,6 +55,15 @@ RSpec.describe 'A cup of coffee' do
       # You should include an explanation why the example is still pending
       pending 'color not implemented yet'
       expect(coffee.color).to be(:light)
+    end
+
+    # Pending example with expectation to be skipped
+    it 'is cooler than 200F' do
+      # Including the pending helper means that any code below it will be skipped
+      # Any code above it is still expected to pass
+      # You should include an explanation why the example is still pending
+      pending 'temperature not implemented yet'
+      expect(coffee.temp).to be < (200)
     end
   end
 end
