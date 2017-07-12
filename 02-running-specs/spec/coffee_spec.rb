33d2d769839bde6a2457628f76d188f792c69fc6
SeanDMR <sean@flyinggrizzly.io>
2017-07-12 20:34:30 +0100

    01-02: expand pending test examples with expecations being skipped using pending helper

--- 02-running-specs/spec/coffee_spec.rb
+++ 02-running-specs/spec/coffee_spec.rb
@@ -26,8 +26,14 @@ RSpec.describe 'A cup of coffee' do
     expect(coffee.price).to eql(1.00)
   end
 
-  # Pending example
-  it 'is cooler than 200F'
+  # Pending example with expectation to be skipped
+  it 'is cooler than 200F' do
+    # Including the pending helper means that any code below it will be skipped
+    # Any code above it is still expected to pass
+    # You should include an explanation why the example is still pending
+    pending 'temperature not implemented yet'
+    expect(coffee.temp).to be < (200)
+  end
 
   # context is an alias for describe, but makes it clear when describing a subset of behaviors
   # Tagged: run just this example with `rspec --tag milky:true`
@@ -36,7 +42,14 @@ RSpec.describe 'A cup of coffee' do
     it 'costs $1.25' do
       expect(coffee.price).to eql(1.25)
     end
-    # Pending example
-    it 'is light in color'
+
+    # Pending example with expectation to be skipped
+    it 'is light in color' do
+      # Including the pending helper means that any code below it will be skipped
+      # Any code above it is still expected to pass
+      # You should include an explanation why the example is still pending
+      pending 'color not implemented yet'
+      expect(coffee.color).to be(:light)
+    end
   end
 end
