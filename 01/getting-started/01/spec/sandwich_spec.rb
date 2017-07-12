71ba9772151ec5ba3557c59e1b4a502caab8d73c
SeanDMR <smr48@bath.ac.uk>
2017-07-12 14:04:42 +0100

    Revert "01-01: REFACTOR: use RSpec before hook to extract common sandwich instantiation code"
    This reverts commit ad55f14c5bbc0237fa3b2515f5d0b0d1aaf2d33c.

--- 01/getting-started/01/spec/sandwich_spec.rb
+++ 01/getting-started/01/spec/sandwich_spec.rb
@@ -6,16 +6,16 @@
 Sandwich = Struct.new(:taste, :toppings)
 
 RSpec.describe 'An ideal sandwich' do
-  before { @sandwich = Sandwich.new('delicious', []) }
-
   it 'is delicious' do
-    taste = @sandwich.taste
+    sandwich = Sandwich.new('delicious', [])
+    taste = sandwich.taste
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'cheese'
-    toppings = @sandwich.toppings
+    sandwich = Sandwich.new('delicious', [])
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
     # check out this inverted expectation with .not_to
     expect(toppings).not_to be_empty 
   end
