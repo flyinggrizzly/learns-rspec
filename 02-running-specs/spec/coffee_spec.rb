7de651fd162046f64f85ec30202b43cfff815cdc
SeanDMR <smr48@bath.ac.uk>
2017-07-12 14:37:38 +0100

    01-02: RED: failing coffee spec

---
+++ 02-running-specs/spec/coffee_spec.rb
@@ -0,0 +1,29 @@
+class Coffee
+  def ingredients
+    @ingredients ||= []
+  end
+
+  def add(ingredient)
+    ingredients << ingredient
+  end
+
+  def price
+    1.00
+  end
+end
+
+RSpec.describe 'A cup of coffee' do
+  let(:coffee) { Coffee.new }
+
+  it 'costs $1' do
+    expect(coffee.price).to eql(1.00)
+  end
+
+  # context is an alias for describe, but makes it clear when describing a subset of behaviors
+  context 'with milk' do
+    before { coffee.add :milk }
+    it 'costs $1.25' do
+      expect(coffee.price).to eql(1.25)
+    end
+  end
+end
