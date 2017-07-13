9833df8f6b20656bb203c9ae15093703d7c81023
SeanDMR <sean@flyinggrizzly.io>
2017-07-13 19:38:05 +0100

    02-01: GREEN: application responds to post method

--- part-02/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ part-02/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -1,9 +1,14 @@
 require 'rack/test'
 require 'json'
+require_relative '../../app/api'
 
 module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
+
+    def app
+      ExpenseTracker::API.new
+    end
 
     it 'records submitted expenses' do
       coffee = {
@@ -12,6 +17,7 @@ module ExpenseTracker
         'date'   => '2017-06-10'
       }
 
+      # test that posting doesn't just crash the app by doing it
       post '/expenses', JSON.generate(coffee)
     end
   end
