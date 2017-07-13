5d01f7a4cb305d64eef6ff1fb4901a0cb387bd83
SeanDMR <sean@flyinggrizzly.io>
2017-07-13 19:48:48 +0100

    02-01: GREEN: api returns a response with the right content

--- part-02/expense_tracker/app/api.rb
+++ part-02/expense_tracker/app/api.rb
@@ -5,6 +5,8 @@ module ExpenseTracker
   class API < Sinatra::Base
 
     post '/expenses' do
+      # write just enough code to get the specs to pass
+      JSON.generate('expense_id' => 42)
     end
 
   end
