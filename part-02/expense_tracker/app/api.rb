df6f6ddc0045693aa9b60c2a7d269f5b45505f78
SeanDMR <smr48@bath.ac.uk>
2017-07-14 16:41:39 +0100

    02-01: RED: fix http error, but not getting expected response body

--- part-02/expense_tracker/app/api.rb
+++ part-02/expense_tracker/app/api.rb
@@ -6,5 +6,10 @@ module ExpenseTracker
     post '/expenses' do
       JSON.generate('expense_id' => 42)
     end
+
+    get '/expenses/:date' do
+      # return empty JSON to fix HTTP error in tests
+      JSON.generate([])
+    end
   end
 end
