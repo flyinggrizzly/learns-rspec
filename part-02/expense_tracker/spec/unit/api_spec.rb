0da210e5262a55772604bc8d4a84661ff18e49b4
SeanDMR <sean@flyinggrizzly.io>
2017-07-14 23:53:35 +0100

    02-05: GREEN: specify that response codes for POST should be 200 if given valid input

--- part-02/expense_tracker/spec/unit/api_spec.rb
+++ part-02/expense_tracker/spec/unit/api_spec.rb
@@ -42,7 +42,16 @@ module ExpenseTracker
           expect(parsed).to include('expense_id' => 417)
         end
 
-        it 'responds with a 200 (OK)'
+        it 'responds with a 200 (OK)' do
+          expense = { 'liveware' => 'problem' }
+
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          post '/expenses', JSON.generate(expense)
+          expect(last_response.status).to eq(200)
+        end
       end
 
       context 'when the expense fails validation' do
