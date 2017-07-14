49bdf1e9a44f36736a9c1cc0764907a65ab266be
SeanDMR <sean@flyinggrizzly.io>
2017-07-14 23:44:16 +0100

    02-05: RED: example specifying transaction id be returned complete but failing

--- part-02/expense_tracker/spec/unit/api_spec.rb
+++ part-02/expense_tracker/spec/unit/api_spec.rb
@@ -1,10 +1,47 @@
 require_relative '../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
+  # Define a dummy response class for the API
+  # Looks nothing like a real response would
+  # but that can be useful because it won't
+  # be confused with a real response. Doesn't
+  # matter what the response looks like, we're
+  # only interested that it responds for the
+  # moment.
+  RecordResult = Struct.new(:success?,
+                            :expense_id,
+                            :error_message)
+
   RSpec.describe API do
+    include Rack::Test::Methods
+
+    # Create a double/mock of the Ledger class (yet to be written)
+    # for testing with the instance_double method
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
+    def app
+      API.new(ledger: ledger)
+    end
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
-        it 'returns the expense id'
+        it 'returns the expense id' do
+          expense = { 'gravitas' => 'shortfall' }
+
+          # Use the `allow` method from rspec-mocks so 
+          # we can configure the behavior of the dummy
+          # ledger object
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+
+          # and now, *finally*, we start testing
+          post '/expenses', JSON.generate(expense)
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('expense_id' => 417)
+        end
+
         it 'responds with a 200 (OK)'
       end
 
