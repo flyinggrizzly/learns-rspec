63a3acc90a6b2f707b96b6d05fa364030de270fc
SeanDMR <smr48@bath.ac.uk>
2017-07-14 16:23:02 +0100

    02-01: RED: API doesn't return transaction ID after post to /expenses

--- part-02/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ part-02/expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -20,6 +20,10 @@ module ExpenseTracker
       post '/expenses', JSON.generate(coffee)
       # test that the post's response is OK
       expect(last_response.status).to eq(200)
+
+      # test that we get a transaction ID back for future use
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Intenger))
     end
   end
 end
