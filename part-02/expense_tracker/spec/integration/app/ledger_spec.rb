336c875f7d819e50fb8f2d82051d172eeabb0985
SeanDMR <sean@flyinggrizzly.io>
2017-07-23 13:04:55 +0100

    02-06: RED: database is not recording expenses

--- part-02/expense_tracker/spec/integration/app/ledger_spec.rb
+++ part-02/expense_tracker/spec/integration/app/ledger_spec.rb
@@ -3,7 +3,7 @@ require_relative '../../../config/sequel'
 require_relative '../../support/db'
 
 module ExpenseTracker
-  RSpec.describe Ledger do
+  RSpec.describe Ledger, :aggregate_failures do
     let(:ledger) { Ledger.new }
     let(:expense) do
       {
@@ -19,7 +19,7 @@ module ExpenseTracker
           result = ledger.record(expense)
           expect(result).to be_success
           expect(DB[:expenses].all).to match [a_hash_including(
-            id: result.expesne_id,
+            id: result.expense_id,
             payee: 'Starbucks',
             amount: 5.75,
             date: Date.iso8601('2017-06-10')
